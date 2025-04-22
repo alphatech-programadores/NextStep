c454f8c21e773712d45331fa64a1cab90414b1f0
samuelisimo <ramueljb@gmail.com>
2025-04-22 16:55:22 -0600

    manage ahora crea el app

--- backend/manage.py
+++ backend/manage.py
@@ -1,12 +1,3 @@
-from flask import Flask
-from extensions import db, migrate
-from models import user  # importa todos tus modelos aquí
+from app import create_app
 
-app = Flask(__name__)
-app.config.from_object("config.Config")
-
-db.init_app(app)
-migrate.init_app(app, db)
-
-if __name__ == "__main__":
-    app.run()
+app = create_app()
