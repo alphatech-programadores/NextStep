ba8754782417a7b70d7672fe6d1959b75c2c6c2f
samuelisimo <ramueljb@gmail.com>
2025-04-22 17:47:24 -0600

    Auto upgradear base de datos

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,7 @@
 import os
-import token
 
 class Config:
     BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
-    confirm_url = f"{BASE_URL}/confirm/{token}"
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///nextstep.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -13,4 +11,3 @@ class Config:
     MAIL_USERNAME = 'c6cd4bf2bd27a0'
     MAIL_PASSWORD = 'dc1b8cb5351ced'
     MAIL_DEFAULT_SENDER = 'noreply@nextstep.com'
-
--- backend/manage.py
+++ backend/manage.py
@@ -1,3 +1,8 @@
 from app import create_app
+from extensions import db
+from flask_migrate import upgrade
 
 app = create_app()
+
+with app.app_context():
+    upgrade()
