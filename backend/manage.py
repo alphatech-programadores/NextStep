9d2d3d3c49a82d829ffb9e4ebc2c2c9fd99ccb64
samuelisimo <ramueljb@gmail.com>
2025-04-22 18:22:33 -0600

    delete alembic

--- backend/manage.py
+++ backend/manage.py
@@ -2,8 +2,13 @@ from app import create_app
 from extensions import db, migrate
 from flask_migrate import upgrade
 
+from sqlalchemy import text
+
+
 app = create_app()
 
 # Aplica migraciones automáticamente al arrancar (solo mientras no tienes shell)
+
 with app.app_context():
+    db.engine.execute(text("DROP TABLE IF EXISTS alembic_version"))
     upgrade()
