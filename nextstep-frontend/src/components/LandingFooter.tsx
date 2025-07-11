29b52a77bb96eec8a8e887f0fcd5609871460e99
alphatech-programadores <alphatech.programacion.6im9@gmail.com>
2025-07-11 17:52:08 -0600

    Delete nextstep-frontend directory

--- nextstep-frontend/src/components/LandingFooter.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles/landing.module.scss";
-
-export default function LandingFooter() {
-    return (
-        <footer className={styles.footer}>
-            © {new Date().getFullYear()} NextStep. Todos los derechos reservados.
-        </footer>
-    );
-}
